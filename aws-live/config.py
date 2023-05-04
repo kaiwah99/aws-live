55b6c368eb07634f9a78c2450294f8bb1baa1065
kaiwah99 <132491212+kaiwah99@users.noreply.github.com>
2023-05-05 00:15:29 +0800

    Update config.py

--- aws-live/config.py
+++ aws-live/config.py
@@ -1,4 +1,4 @@
-customhost = "airasiadb1.cjpfkv9am7a4.us-east-1.rds.amazonaws.com"
+customhost = "airasiadb.cjpfkv9am7a4.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "123123123"
-customdb = "airasiadb1"
+customdb = "airasiadb"
